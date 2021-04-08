4201c6b7ff36b0d5408a2e32f5080222b2210ac5
nov segal <novit100@gmail.com>
2021-04-08 14:50:38 +0300

    commit 08/04

---
+++ src/unittests/stam.java
@@ -0,0 +1,15 @@
+package unittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class stam {
+
+	@Test
+	public void test() {
+		System.out.println("hey");
+		assertTrue("error accured", 1==1);
+	}
+
+}
